557ef2500d110c2d78b97411cf4da3cee86a282a
Oliveira-Renato <renato.pritf@gmail.com>
2024-09-24 23:12:57 -0300

    endpoint to list all countries done

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //Endpoint to list countries
+app.get('/countries', async (req, res) => {
+  try {
+    const response = await axios.get('https://date.nager.at/api/v3/AvailableCountries');
+    res.json(response.data)
+  } catch (error) {
+    res.status(500).json({ error: 'Error searching for countries' })
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
